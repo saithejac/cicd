d3f73793b9b081abe99cdb50ac5ea28bb7124769
saithejac <34050020+saithejac@users.noreply.github.com>
2025-07-24 17:32:09 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello from C++ insidesss Docker!" << std::endl;
+    std::cout << "Hello from C++ inside Docker cicd!" << std::endl;
     return 0;
 }
